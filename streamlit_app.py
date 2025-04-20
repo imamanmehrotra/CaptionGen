6146208e31367c25e7cc58d1359f8cd291f9ead0
CuriousTechMonk <amansammehrotra@gmail.com>
2025-04-21 00:11:36 +0800

    Update streamlit_app.py with the latest models

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,7 +114,8 @@
 #                 ],
 #             }
 #         ],
-#         model="llama-3.2-90b-vision-preview",
+#         #model="llama-3.2-90b-vision-preview",
+#         model = "meta-llama/llama-4-scout-17b-16e-instruct",
 #         response_format={"type": "json_object"},
 #         stop=None,
 #         top_p=1,
